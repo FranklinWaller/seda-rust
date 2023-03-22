a09c1f75ff32844edb0e0d17cab9eea7449f49b1
Franklin Waller <fr.l.waller@gmail.com>
2023-03-22 14:47:13 +0100

    ref(wasm): Do not pass the private key to the WASM binary

--- wasm/consensus/src/tasks/batch.rs
+++ wasm/consensus/src/tasks/batch.rs
@@ -1,17 +1,7 @@
 use clap::Args;
 use seda_runtime_sdk::{
     log,
-    wasm::{
-        bn254_sign,
-        call_self,
-        chain_view,
-        p2p_broadcast_message,
-        shared_memory_get,
-        shared_memory_set,
-        Bn254PrivateKey,
-        Promise,
-        CONFIG,
-    },
+    wasm::{bn254_sign, call_self, chain_view, get_oracle_contract_id, p2p_broadcast_message, Promise},
     Level,
     PromiseStatus,
 };
@@ -22,18 +12,10 @@ pub struct Batch;
 impl Batch {
     pub fn handle(self) {
         log!(Level::Debug, "Batch Handle");
-        // TODO temp work around while env bug exists
-        shared_memory_set(
-            "private_key_bytes",
-            CONFIG
-                .keypair_bn254
-                .private_key
-                .to_bytes()
-                .expect("Bn254 private should exist"),
-        );
+
         chain_view(
             seda_runtime_sdk::Chain::Near,
-            &CONFIG.contract_account_id,
+            get_oracle_contract_id(),
             "compute_merkle_root",
             Vec::new(),
         )
@@ -45,33 +27,17 @@ impl Batch {
 #[no_mangle]
 fn batch_step_1() {
     log!(Level::Debug, "Batch Step 1");
-    // TODO temp work around while env bug exists
-    let pk_bytes = shared_memory_get("private_key_bytes");
-    let pk = Bn254PrivateKey::try_from(pk_bytes.as_slice()).expect("TODO");
+
     let result = Promise::result(0);
     match result {
         PromiseStatus::Fulfilled(Some(bytes)) => {
-            log!(Level::Debug, "{bytes:?}");
-            let result = bn254_sign(&bytes, &pk);
+            let result = bn254_sign(&bytes);
             let hex = hex::encode(result.to_compressed().expect("TODO"));
-            p2p_broadcast_message(hex.into_bytes())
-                .start()
-                .then(call_self("batch_step_2", vec![]));
+
+            log!(Level::Debug, "Broadcasting signed batch: 0x{hex}");
+
+            p2p_broadcast_message(hex.into_bytes()).start();
         }
         other => log!(Level::Debug, "{other:?}"),
     };
-}
-
-#[no_mangle]
-fn batch_step_2() {
-    let result = Promise::result(0);
-    log!(Level::Debug, "Batch Step 2 {result:?}");
-    // TODO p2p_broadcast_message doesn't set this yet...
-    // match result {
-    //     PromiseStatus::Fulfilled(Some(bytes)) => {
-    //         let str = String::from_bytes_vec(bytes).expect("TODO");
-    //         log!(seda_runtime_sdk::Level::Debug, "Success: {str}");
-    //     }
-    //     other => log!(Level::Debug, "{other:?}"),
-    // };
 }
